2fa5da82db3efd9ecff1fda0ef9677ff0eec3974
njb69 <46762303+njb69@users.noreply.github.com>
2019-02-14 14:49:53 -0500

    Update card.h

--- war/card.h
+++ war/card.h
@@ -37,15 +37,22 @@ enum Suit
 class card
 {
 	private:
-	Suit suit;
-	Rank rank;
+	unsigned char cardData;//8 bit storage for the card's suit and rank
 	
 	public:
-	card(int, int);//constructor: accepts int as index for Rank and int as index for Suit and makes card with cooresponding values
+	card(Rank r, Suit s)//constructor: accepts int as index for Rank and int as index for Suit and makes card with cooresponding values
+		:cardData(static_cast<unsigned char>(s) << 4 | static_cast<unsigned char>(r))
+		{}//cardData now contains a bit pattern of ssssrrrr. we're currently only using 2 bits for suit but we have 4 available
+	
 	Rank getRank()const;
 	Suit getSuit()const;
 	Sprite getSprite()const;
 	~card();
+	
+	bool operator<(Card a, Card b);
+	bool operator>(Card a, Card b);
+	bool operator==(Card a, Card b);
+	bool operator!=(Card a, Card b);
 };
 
 #endif
