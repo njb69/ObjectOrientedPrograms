6d1a806fecd12d22c6153225e958b0bf95289745
njb69 <46762303+njb69@users.noreply.github.com>
2019-02-28 21:43:43 -0500

    Update card.cpp

--- war/card.cpp
+++ war/card.cpp
@@ -1,17 +1,17 @@
 #include "card.h"
 
-Rank card::getSuit()const
+Rank normalCard::getSuit()const
 {
 	unsigned char suitOnly = static_cast <unsigned char> (cardData >> 4);//cut off bits representing rank in cardData, and shifts suit bits right, where they can be interpreted as int 	
 	return suitOnly;
 }
 
-Suit card::getRank()const
+Suit normalCard::getRank()const
 {
 	return static_cast <int> (cardData & 0xf;);//use bitwise and to get last 4 bits of cardData with 1111 (15 or hex f) 
 }
 
-Sprite card::getSprite()const
+Sprite normalCard::getSprite()const
 {
 	Texture aceHeartsTexture;
 	aceHeartsTexture.loadFromFile("data/images/aceHearts.png");
@@ -20,33 +20,36 @@ Sprite card::getSprite()const
 	//etc
 }
 
-bool card::operator<(Card a, Card b)
+bool PlayingCard::operator<(PlayingCard a, PlayingCardCard b)
 {
-  if (a.get_suit() < b.get_suit())
-  {
-    return true;
-  }
- else
-  if (b.get_suit() <= a.get_suit())
-  {
-    return false;
-  }
+	if(a.is_joker() || b.is_joker())
+	{
+		if(a.is_joker() && b.is_joker())
+			{return false;}
+		else
+			{return a.is_joker()}
+	}
+		
+	return a.get_as_standard().get_rank() < b.get_as_standard().get_rank();
 }
 
-bool card::operator>(Card a, Card b)
+bool PlayingCard::operator>(PlayingCard a, PlayingCardCard b)
 {
   return b < a;
 }
 
-bool card::operator==(Card a, Card b)
+bool PlayingCard::operator==(PlayingCard a, PlayingCardCard b)
 {
-  return a.get_rank() == b.get_rank() &&
-         a.get_suit() == b.get_suit();
+  if(a.is_joker() && b.is_joker())
+	{return true;}
+else
+  if(a.is_joker() || b.is_joker())
+	{return false;}
+  
+  return (a.get_as_standard().get_rank() == b.get_as_standard().get_rank());
 }
 
-bool card::operator!=(Card a, Card b)
+bool PlayingCard::operator!=(PlayingCard a, PlayingCardCard b)
 {
   return !(a == b);
 }
-
-
