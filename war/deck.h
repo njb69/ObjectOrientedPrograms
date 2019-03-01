61902ec9df6ff683762c13dff0574e41acb76a68
njb69 <46762303+njb69@users.noreply.github.com>
2019-02-28 21:46:25 -0500

    Update deck.h

--- war/deck.h
+++ war/deck.h
@@ -10,14 +10,14 @@
 class deck
 {
 	private:
-	std::vector<card>theDeck;
+	std::vector<PlayingCard>theDeck;
 	
 	public:
 	deck(bool, bool);//constructor must be defined so all cards are made, 1st bool for aceHigh, 2nd for jokers(true = yes)
 	
 	void shuffle();//shuffles the deck
 	
-	card& getCard(int);//returns reference to card at specified index
+	PlayingCard& getCard(int);//returns reference to card at specified index
 	
 	~deck();
 };
