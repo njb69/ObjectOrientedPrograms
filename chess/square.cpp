235ee9dafb665fa91637cf8f4ec260d66f8884a3
njb69 <46762303+njb69@users.noreply.github.com>
2019-04-09 22:07:43 -0400

    Update square.cpp

--- chess/square.cpp
+++ chess/square.cpp
@@ -4,18 +4,66 @@ void square::setPiece(pieceType type, pieceColor color, int x, int y)
 {
 	this->type = type;
 	this->color = color;
-	xPos = x;
-	yPos = y;
+	this->xPos = x;
+	this->yPos = y;
+	
+	if(color == BLACK)
+	{
+		switch(type)
+		{
+			case PAWN : card.setCard(Two, Black); break;
+			case ROOK : card.setCard(Nine, Black); break;
+			case BISHOP : card.setCard(Ten, Black); break;
+			case KNIGHT : card.setCard(Jack, Black); break;
+			case QUEEN : card.setCard(Queen, Black); break;
+			case KING : card.setCard(King, Black); break;
+		}
+	}
+	else
+	if(color == WHITE)
+	{
+		switch(type)
+		{
+			case PAWN : card.setCard(Two, White); break;
+			case ROOK : card.setCard(Nine, White); break;
+			case BISHOP : card.setCard(Ten, White); break;
+			case KNIGHT : card.setCard(Jack, White); break;
+			case QUEEN : card.setCard(Queen, White); break;
+			case KING : card.setCard(King, White); break;
+		}
+	}
 }
 
-void square::setSquare(square* destination)
+short int square::captureSquare(square* destination)
 {
-	destination->type = this->type;
-	destination->color = this->color;
+	destination->setType(this->type);
+	destination->setColor(this->color);
+	
+	return 0;
+	
+	if(destination->getType() == KING)
+	{
+		return 1;
+	}
 }
 
-void square::clear()
+void square::captureCard(square* destination)
 {
-	this->color = none;
-	this->type = none;
+	if((destination->getCard()).getRank() != NoRank)
+	{
+		if(destination->getColor() == BLACK)
+		{
+			standardCard temp = destination->getCard();
+			players::playerWhite.addCard(temp);
+		}
+		else
+		{
+			standardCard temp = destination->getCard();
+			players::playerBlack.addCard(temp);
+		}
+	}
+	destination->getCard() = this->getCard();
+			
+	this->getCard() = staticObjects::valuelessCard;
 }
+
